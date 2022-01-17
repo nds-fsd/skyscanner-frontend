5562ba27ecc0f4ec46433ac81d4f43c8b3ebd0b3
Eric <eric.capella.1r@gmail.com>
2022-01-17 17:51:03 +0100

    useform implementado sin fetch, en console.log funciona

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,17 +1,44 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Search = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+    // fetch('http://localhost:5001/flights', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(data)
+    // })
+    //   .then(response => {
+    //     if (!response.ok) throw new Error("Couldn't upload")
+    //     return response.json();
+    //   })
+    //   .then(json => {
+    //     alert(JSON.stringify(json));
+    //   })
+    //   .catch(error => {
+    //     alert(error);
+    //   })
+    console.log(data);
+  };
+
     return (
         <div>
             <p>Buscar</p>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label for="from">From</label>
-                <input type="search" />
+                <input type="search" {...register("from")}/>
                 <label for="To">To</label>
-                <input type="search" />
-                <label for="To">When</label>
-                <input type="date" />
-                <button>See flights</button>
+                <input type="search" {...register("to")}/>
+                <label for="To">Departure date</label>
+                <input type="date" {...register("dedate")}/>
+                <label for="To">Return date</label>
+                <input type="date" {...register("arrdate")}/>
+                <input type="submit" />
+                {/* <button>See flights</button> */}
             </form>
         </div>
     );
