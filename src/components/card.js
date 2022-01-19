b3955d1212442df2ac4aa542850c4fbcf5175a90
Carolina Marianela <gallegos.carolina@outlook.com>
2022-01-19 20:52:08 +0100

    información recibida del backend

--- src/components/card.js
+++ src/components/card.js
@@ -2,14 +2,20 @@ import React from "react";
 import './card.css';
 
 function FlightCard() {
+    const Card = ({from, to, dedate, arrdate, price, airline}) => {
     return (
         <div className="card-total">
-            <header className="yellow">vueling</header>
-            <div className="name-result">
-                Resultados
-            </div>
-
+                <header className="yellow"><p>Compañia: {airline}</p></header>
+            <Card className="name-result">
+            <p>Desde: {from}</p>
+            <p>A: {to}</p>
+            <p>Fecha de ida{dedate}</p>
+            <p>Fecha de vuelta: {arrdate}</p>
+            <p>Precio: {price} euros</p>
+            <p>Compañia: {airline}</p>
+            </Card>
         </div>
-    )
-}
-export default FlightCard
+        )
+  
+    }}
+  export default FlightCard;
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,19 +1,38 @@
 import React from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 import FlightCard from "../components/card"
 import SearchResult from "../components/search-result"
 import Sidebar from "../components/sidebar"
 import './Results.css'
+import { useLocation } from 'react-router'
 
 function Results () {
-    return (
-    <section >
-        <SearchResult className="from-to"></SearchResult>
-        <div className="total-page">
-            <Sidebar className="sidebar-content"></Sidebar>
-             <FlightCard className="card-content"></FlightCard>
-        </div>
-    </section>
-    )
-}
+    const location = useLocation();
+    const [params] = useState(location.pathname.split("/"));
+    const [data, setData] = useState ([]);
+    console.log(params)
+    useEffect(() => {
+        const request = fetch(`http://localhost:5001/flights/search?from=${params[2]}&to=${params[3]}&dedate=${params[4]}&arrdate=${params[5]}`)
+        request.then(response => {
+        if (!response.ok) throw new Error("Couldn't ")
+         return response.json();
+        })
+        .then((json) => {
+        console.log(json);
+        setData(json)
+        }
+        );
+    })
 
+    return (
+        <section >
+            <SearchResult className="from-to"></SearchResult>
+            <div className="total-page">
+            <Sidebar className="sidebar-content"></Sidebar>
+            {data.map((flight) => (<FlightCard className="card-content" from={flight.from} to={flight.to} dedate={flight.dedate} arrdate={flight.arrdate} price={flight.price} airline={flight.airline}/>))}
+            </div>
+        </section>
+    )
+    };  
 export default Results
