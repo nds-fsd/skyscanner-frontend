c579b8a7faee9eb0a375f142d791d8920944eec3
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2022-01-31 21:24:27 +0100

    usertoken authorization

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useForm } from "react-hook-form";
-import styles from './loginForm.module.css'
+import { useNavigate } from "react-router-dom";
+import styles from './loginForm.module.css';
 
 const LoginForm = () => {
-    const { register, handleSubmit} = useForm();
-    
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, formState: { errors }} = useForm();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("user-session")?.token;
+        if (token) navigate("/");
+    }, []);
+
+    const onSubmit = (data) => {
+        fetch("*URL_BACKEND*", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                "content-type": 'application/json',
+                /*"authorization": `Bearer ${getUserToken()}`*/
+            },
+            body: JSON.stringify(data)
+        })
+        .then(userSession => {
+            localStorage.setItem("user-session", userSession);
+            navigate("/");
+        }).catch(error => {
+          console.error(error);
+        });
+      };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <div className={styles.inputDiv}>
                 <label>Introduce tu nombre de usuario</label>
-                <input {...register("username")} placeholder="Usuario" type="text"/>
+                <input {...register("username", { required: true })} placeholder="Usuario" type="text"/>
+                {errors.email && <span>This field is required</span>}
             </div>
             <div className={styles.inputDiv}>
                 <label>Introduce tu contraseña</label>
-                <input {...register("password")} placeholder="Contraseña" type="password" />
+                <input {...register("password", { required: true })} placeholder="Contraseña" type="password" />
+                {errors.password && <span>This field is required</span>}
             </div>
             <input type="submit" />
         </form>
