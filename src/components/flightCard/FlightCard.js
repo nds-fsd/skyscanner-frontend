234d06a099ca697e59df3a0f728954d4e966b163
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2022-02-27 10:58:06 +0100

    flightCard modifications

--- src/components/flightCard/FlightCard.js
+++ src/components/flightCard/FlightCard.js
@@ -6,14 +6,21 @@ import ryanairLogo from '../../images/airline_logos/ryaniar_logo.png';
 import aireuropaLogo from '../../images/airline_logos/air_europa_logo.png';
 import iberiaLogo from '../../images/airline_logos/iberia_logo.png';
 import bintercanariasLogo from '../../images/airline_logos/binter_logo.webp';
+import moment from 'moment';
 
 
 function FlightCard(props) {
     const {onClick, flight} = props;
-    const {from, to, price, airline, _id} = flight;
+    const {from, to, price, airline, dedate, flighttime, _id} = flight;
 
-    console.log(airline.replace(/\s/g, '').toLowerCase());
-   
+    console.log()
+    
+    const minutsToHHMM = (minutes) => {
+        var m = minutes % 60;
+        var h = (minutes-m)/60;
+        return h.toString() + ":" + (m<10?"0":"") + m.toString();
+    }
+
     return (
         <div className="card">
             <div className={`${airline.replace(/\s/g, '').toLowerCase()} card-color `} />
@@ -26,22 +33,34 @@ function FlightCard(props) {
                     {airline.replace(/\s/g, '').toLowerCase() === "bintercanarias" && <img className="airline-logo" alt={airline} src={bintercanariasLogo}/>}
                 </div>
                 <div className="flight-info">
-                    <div className="from">
-                        <p>{from}</p>
+                    <div className="from-to">
+                        <p className="city-name">{from}</p>
+                        <span className="de-arr-time">{moment(dedate).format('LT')}</span>
                     </div>
                     <div className="trip">
+                        
                         <img src={cardimage} alt="trip-icon"/>
+                        <div className="duration">
+                            <span>DIRECT</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="clock-icon" fill="E5E5E5" width="10px" height="10px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span>{minutsToHHMM(flighttime)}</span>
+                        </div>
                     </div>
-                    <div className="to">
-                        <p>{to}</p>
+                    <div className="from-to">
+                        <p className="city-name">{to}</p>
+                        <span className="de-arr-time">{(moment(Date.parse(dedate)+(flighttime*60000)).format('LT'))}</span>
                     </div>
                 </div>
                 <div className="divider"></div>
                 <div className="actions">
-                    <p>{price} €</p>
-                        <div className="buttonSelect" onClick={() => onClick(_id)}>
-                            <p>Select flight</p>
-                        </div>
+                    <p className="price">{price} €</p>
+                    <span className="price-info">Precio para 1 pasajero</span>
+                    {flight.seats < 5 && <span className="seats-info">{flight.seats === 1 ? "Sólo queda 1 asiento disponible !" : flight.seats + " asientos disponibles !"}</span>}
+                    <button className="buttonSelect" onClick={() => onClick(_id)}>
+                        Select flight
+                    </button>
                 </div>
             </div>
         </div>
