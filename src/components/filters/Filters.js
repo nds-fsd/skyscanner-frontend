00628274c439b705727c891334a79c5bcd3a179b
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2022-02-27 19:00:50 +0100

    filters

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -3,9 +3,8 @@ import './filters.css'
 import {useEffect} from 'react';
 
 const Filters = (props) => {
-    const airlines = ['Vueling', 'Rynair', 'Iberia', 'BinterCanarias'];
+    const airlines = ['Vueling', 'Ryanair', 'Iberia', 'BinterCanarias'];
     const {filters, setFilters} = props;
-    let airlinesList = [];
 
     const handleChangePrice = (e) => {
         setFilters({...filters, minPrice: e.target.value});
@@ -15,10 +14,11 @@ const Filters = (props) => {
         setFilters({...filters, minHour: e.target.value});
     }
 
-    const handleClickAirline = (airline) => {
-        if(airline !== undefined) {
-            airlinesList.push(airline);
-            setFilters({...filters, selectedAirlines: airlinesList});
+    const handleClickAirline = (e) => {
+        if(e.target.checked) {
+            setFilters({...filters, selectedAirlines: [...filters.selectedAirlines, e.target.value]});
+        } else {
+            setFilters({...filters, selectedAirlines: filters.selectedAirlines.filter((a) => a !== e.target.value)});
         }
     }
 
@@ -30,43 +30,24 @@ const Filters = (props) => {
                 <div className="hour-select">
                     <select name="hour" onChange={handleChangeHour}>
                         <option value="5:00">5:00 am</option>
-                        <option value="5:30">5:30 am</option>
                         <option value="6:00">6:00 am</option>
-                        <option value="6:30">6:30 am</option>
                         <option value="7:00">7:00 am</option>
-                        <option value="7:30">8:30 am</option>
-                        <option value="8:00">9:00 am</option>
-                        <option value="8:30">9:30 am</option>
-                        <option value="9:00">10:00 am</option>
-                        <option value="9:30">10:30 am</option>
-                        <option value="10:00">11:00 am</option>
-                        <option value="10:30">11:30 am</option>
-                        <option value="11:00">12:00 am</option>
-                        <option value="11:30">12:30 am</option>
-                        <option value="12:00">1:00 pm</option>
-                        <option value="12:30">1:30 pm</option>
-                        <option value="13:00">2:00 pm</option>
-                        <option value="13:30">2:30 pm</option>
+                        <option value="8:00">8:00 am</option>
+                        <option value="9:00">9:00 am</option>
+                        <option value="10:00">10:00 am</option>
+                        <option value="11:00">11:00 am</option>
+                        <option value="12:00">12:00 pm</option>
+                        <option value="13:00">1:00 pm</option>
+                        <option value="12:00">2:00 pm</option>
                         <option value="14:00">3:00 pm</option>
-                        <option value="14:30">3:30 pm</option>
                         <option value="15:00">4:00 pm</option>
-                        <option value="16:30">4:30 pm</option>
-                        <option value="17:00">5:00 pm</option>
-                        <option value="17:30">5:30 pm</option>
-                        <option value="18:00">6:00 pm</option>
-                        <option value="18:30">6:30 pm</option>
-                        <option value="19:00">7:00 pm</option>
-                        <option value="19:30">7:30 pm</option>
-                        <option value="20:00">8:00 pm</option>
-                        <option value="20:30">8:30 pm</option>
-                        <option value="21:00">9:00 pm</option>
-                        <option value="21:30">9:30 pm</option>
-                        <option value="22:00">10:00 pm</option>
-                        <option value="22:30">10:30 pm</option>
-                        <option value="23:00">11:00 pm</option>
-                        <option value="23:30">11:30 pm</option>
-                        <option value="00:00">00:00 pm</option>
-                        <option value="00:30">00:30 pm</option>
+                        <option value="16:00">5:00 pm</option>
+                        <option value="17:00">6:00 pm</option>
+                        <option value="18:00">7:00 pm</option>
+                        <option value="19:00">8:00 pm</option>
+                        <option value="20:00">9:00 pm</option>
+                        <option value="21:00">10:00 pm</option>
+                        <option value="22:00">11:00 pm</option>
                     </select>
                     <i></i>
                 </div>
@@ -74,7 +55,7 @@ const Filters = (props) => {
             <div className="filter">
                 <h4 className="filter-title">Precio</h4>
                 <h5 className="filter-subtitle">Precio mínimo del vuelo</h5>
-                <input type="range" min="0" max="500" step="10" className="price-range" onChange={handleChangePrice}/>
+                <input type="range" min="0" max="500" className="price-range" onChange={handleChangePrice}/>
                 <p className="min-price"><span>{filters.minPrice}</span>€</p>
             </div>
             <div className="filter">
@@ -82,7 +63,7 @@ const Filters = (props) => {
                 {airlines.map((airline) => {
                     return (
                         <div className="checkbox">
-                            <input type="checkbox" id={airline} className="airlne-checkbok" onClick={() => handleClickAirline(airline)} />
+                            <input type="checkbox" id={airline} value={airline} className="airlne-checkbok" onClick={handleClickAirline} />
                             <label className="airline-checkbox" for={airline}>{airline}</label>
                         </div>
                     );
--- src/pages/resultsPage/ResultsPage.js
+++ src/pages/resultsPage/ResultsPage.js
@@ -7,12 +7,12 @@ import Sidebar from "../../components/sideBar/Sidebar";
 import customFetch from '../../api';
 import mockFlights from '../../data/flights-data.json';
 import TopBar from "../../components/topBar/TopBar";
+import Footer from "../../components/footer/Footer";
 import { useParams } from 'react-router';
-
+import moment from 'moment';
 
 function ResultsPage () {
     const [flights, setFlights] = useState(mockFlights);
-    const [selectedFlight, setSelectedFlight] = useState();
     const [filteredFlights, setFilteredFlights] = useState([]);
     const [filters, setFilters] = useState({
         minPrice: 0,
@@ -22,11 +22,43 @@ function ResultsPage () {
     const {from, to, dedate, retdate, deid, passangers} = useParams();
     const [order, setOrder] = useState();
 
-    useEffect(() => {
-        if (filters.minPrice !== 0 || filters.minHour !== 0 || filters.selectedAirlines.length !== 0) {
-            setFilteredFlights(flights.filter((flight) => flight.price > filters.minPrice && /* hour filter && */ filters.selectedAirlines.includes(flight.airline)))
+    const compareHours = (a, b) => {
+        if(a !== 0) {
+            let bHour = parseInt(moment(b).format('HH:mm').split(":")[0]);
+            let aHour = parseInt(a.split(':')[0]);
+            return aHour < bHour;
         }
-    }, [filters])
+    }
+
+    useEffect(() => {
+        if (filters.minPrice !== 0) {
+            if(filteredFlights.length !== 0) {
+                setFilteredFlights(filteredFlights.filter((flight) => flight.price > filters.minPrice)) 
+            } else {
+                setFilteredFlights(flights.filter((flight) => flight.price > filters.minPrice)) 
+            }
+        }
+    }, [filters.minPrice])
+
+    useEffect(() => {
+        if (filters.minHour !== 0) {
+            if(filteredFlights.length !== 0) {
+                setFilteredFlights(filteredFlights.filter((flight) => compareHours(filters.minHour, flight?.dedate)))
+            } else {
+                setFilteredFlights(flights.filter((flight) => compareHours(filters.minHour, flight?.dedate)))
+            }
+        } 
+    }, [filters.minHour])
+
+    useEffect(() => {
+        if(filters.selectedAirlines.length !== 0) {
+            if (filteredFlights.length !== 0) {
+                setFilteredFlights(filteredFlights.filter((flight) => filters.selectedAirlines.includes(flight.airline)))
+            } else {
+                setFilteredFlights(flights.filter((flight) => filters.selectedAirlines.includes(flight.airline)))
+            }  
+        }
+    }, [filters.selectedAirlines])
 
     /* useEffect( () => {
         customFetch("POST", `flights/search?from=${from}&to=${to}&retdate=${dedate}`)
@@ -71,6 +103,7 @@ function ResultsPage () {
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 };  
