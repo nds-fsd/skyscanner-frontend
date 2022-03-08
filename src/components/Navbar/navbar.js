dcf77ad43075d81fd471be5ba51d987beaa5b4de
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2022-03-08 20:16:36 +0100

    last changes before merge

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,65 +1,41 @@
 import React, {useState, useEffect}from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../files/logo.png";
-import {removeSession} from "../../api/auth";
-import { getUserToken } from "../../api/auth";
-import customFetch from "../../api"
-import "./navbar.css"
-import Avatar from "../avatar/Avatar";
-import jwt_decode from "jwt-decode";
-import mockUser from "../../data/user.json";
+import "../Navbar/navbar.css"
 
 const NavBar = () => {
 
-    const [currentUser, setCurrentUser]= useState(mockUser);
-    const navigate = useNavigate();
+    const [currentUser, setCurrentUser]= useState(undefined);
 
-    /*useEffect(() => {
-        const user = getUserToken();
-        console.log(jwt_decode(user).id)
+    useEffect(() => {
+        const user = localStorage.getItem("token");
         if (user) {
-          customFetch("GET", `profile/${jwt_decode(user).id}`)
-            .then(u => setCurrentUser(u))
-            .catch(err => console.error(err));
+          setCurrentUser(user);
         }
-    }, []);*/
-
-    /*const Logout = () => {
-    removeSession();
-    navigate("/");
-    setCurrentUser(null);
-    }*/
-
+      }, [currentUser]);
+    const Logout = () => {
+        localStorage.removeItem('token');
+        setCurrentUser(null);
+    }
     return (
         <div className="header">
             <div>
-                <Link to="/" >
-                    <img className="logo" src={logo} alt="logo"/>
-                </Link>
+            <Link to="/" ><img className="logo" src={logo} alt="logo"/></Link>
+
             </div>
             {currentUser ? 
             <>
-                <div className="navbar-icon">
-                    <Link to="/profile">
-                        <Avatar user={currentUser} />
-                    </Link>
-                </div>
+            <button className="btn-1"onClick={ Logout }>Logout</button>
             
             </>
             :
             <>
-                <div className="login">
-                    <Link to="/login">
-                        <p>Login</p>
-                    </Link>
-                </div>
-                <div className="register">
-                    <Link to="/signup">
-                        <p>Register</p>
-                    </Link>
-                </div>
+            <div className="login"><Link to="/login"><p>Login</p></Link></div>
+            <div className="register"><Link to="/signup"><p>Register</p></Link></div>
+            
             </>
-        } 
+        }
+            
         </div>
     );
 };
