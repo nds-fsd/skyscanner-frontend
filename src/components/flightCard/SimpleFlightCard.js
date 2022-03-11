6816e8585e575677ee1c18c8cdd378e6610fa448
Carolina Marianela <gallegos.carolina@outlook.com>
2022-03-11 16:07:40 +0100

    commit arreglando los warning

--- src/components/flightCard/SimpleFlightCard.js
+++ src/components/flightCard/SimpleFlightCard.js
@@ -41,7 +41,7 @@ function SimpleFlightCard(props) {
                         <div className="duration">
                             <span>DIRECT</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="clock-icon" fill="E5E5E5" width="10px" height="10px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                             <span>{minutsToHHMM(flighttime)}</span>
                         </div>
