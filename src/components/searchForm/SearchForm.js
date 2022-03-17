29c663428a1cb2d00c24d929eecfd0915b5e8a00
jlcrayo <jlcrayo@gmail.com>
2022-03-17 18:29:48 +0100

    validacion search form

--- src/components/searchForm/SearchForm.js
+++ src/components/searchForm/SearchForm.js
@@ -7,7 +7,9 @@ import { UserContext } from '../../context/userContext';
 const Search = (destination) => {
     const {user} = useContext(UserContext);
     const { register, handleSubmit} = useForm(  
-        {defaultValues: {from:user?.prefairport}}
+        {defaultValues: {from:user?.prefairport},
+        mode: 'onTouched',
+        shouldUseNativeValidation: true}
     );
     const navigate = useNavigate();
     const onSubmit = (data) => {
@@ -22,23 +24,33 @@ const Search = (destination) => {
                 <div className="search-form-inputs">
                     <div className="search-form-group">
                         <label className="search-form-label text-input">From</label>
-                        <input className="search-form-input" type="search" {...register("from")} placeholder="From" />
+                        <input className="search-form-input" type="search" {...register("from", { 
+                        required: "Enter city of origin." , 
+                        })} placeholder="From" />
                     </div>
                     <div className="search-form-group">
                         <label className="search-form-label text-input">To</label>
-                        <input className="search-form-input" type="search" {...register("to")} placeholder="To" />
+                        <input className="search-form-input" type="search" {...register("to",{ 
+                        required: "Enter destination city." , 
+                        })} placeholder="To" />
                     </div>
                     <div className="search-form-group">
                         <label className="search-form-label">Departure date</label>
-                        <input className="search-form-input date-input" type="date" {...register("dedate")} placeholder="09/07/2022"/>
+                        <input className="search-form-input date-input" type="date" {...register("dedate",{ 
+                        required: "Enter a departure date." , 
+                        })} placeholder="09/07/2022"/>
                     </div>
                     <div className="search-form-group">
                         <label className="search-form-label" >Return date</label>
-                        <input className="search-form-input date-input" type="date" {...register("arrdate")} placeholder="09/11/2022"/>
+                        <input className="search-form-input date-input" type="date" {...register("arrdate",{ 
+                        required: "Enter a return date." , 
+                        })} placeholder="09/11/2022"/>
                     </div>
                     <div className="search-form-group">
                         <label className="search-form-label" >Passangers</label>
-                        <input className="search-form-input number-input" type="text" {...register("passangers")} placeholder="#"/>
+                        <input className="search-form-input number-input" type="text" {...register("passangers",{ 
+                        required: "Enter number of passengers." , 
+                        })} placeholder="#"/>
                     </div>
                 </div>
                 <div className="search-form-submit">
